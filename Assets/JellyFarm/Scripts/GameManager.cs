f341770229f8d41d513a2e21f0dae1b8e808f9ec
KwonYJ <dydwns1021@naver.com>
2025-03-15 13:40:27 +0900

    Feat: Add File

---
+++ Assets/JellyFarm/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] jellySpriteList;
+    public string[] jellyNameList;
+    public int[] jellyJelatinList;
+    public int[] jellyPrices;
+    public int[] numPriceList;
+    public int[] clickPriceList;
+
+    public Vector3[] pointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
+}
